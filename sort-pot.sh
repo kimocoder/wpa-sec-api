9ede482848352bb04ae6ef8519d882097e9f2a58
Czechball <david@jenne.cz>
2020-06-08 21:09:13 +0200

    fixed if

--- sort-pot.sh
+++ sort-pot.sh
@@ -20,12 +20,12 @@ else
 	rm temp-pot.txt
 	printf "Comparing old and new potfiles and saving to newsites.txt\n"
 	diff -u old.potfile current.potfile | grep -E "^\+" | tail -n +2 | tr -d "+" > newsites.txt
-	fi
-else
+	else
 	printf "No existing potfiles detected. Current one will get downloaded and saved to current.potfile\n"
 	printf "Downloading potfile...\n"
 	./get-pot.sh "$KEY" > temp-pot.txt
 	printf "Saving cracked handshakes to current.potfile...\n"
 	cat temp-pot.txt | sort | uniq -u -w 12 | cut -d ":" -f 1,3,4 > current.potfile
 	rm temp-pot.txt
+	fi
 fi
