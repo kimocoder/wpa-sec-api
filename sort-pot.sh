c44383a7aac700a8c6cf9bc9247b1d4e1be81462
Czechball <david@jenne.cz>
2020-06-13 01:55:11 +0200

    I have no idea but this fixed something

--- sort-pot.sh
+++ sort-pot.sh
@@ -19,7 +19,7 @@ else
 	cat temp-pot.txt | sort | uniq -w 12 | cut -d ":" -f 1,3,4 > current.potfile
 	rm temp-pot.txt
 	printf "Comparing old and new potfiles and saving to newsites.txt\n"
-	diff -u old.potfile current.potfile | grep -E "^\+" | tail -n +2 | tr -d "+" > newsites.txt
+	diff -ua old.potfile current.potfile | grep -Ea "^\+" | tail -n +2 | tr -d "+" > newsites.txt
 	else
 	printf "No existing potfiles detected. Current one will get downloaded and saved to current.potfile\n"
 	printf "Downloading potfile...\n"
