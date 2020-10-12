85592968192d16587fab1c1168ff838af217232c
Czechball <david@jenne.cz>
2020-10-12 16:59:46 +0200

    added cracked count

--- sort-pot.sh
+++ sort-pot.sh
@@ -11,15 +11,21 @@ else
 			TEMPDATE=$(date -r old.potfile "+%Y-%m-%d_%H-%M-%S")
 			mv old.potfile archive/$TEMPDATE-old.potfile
 		fi
-	echo -e "\e[92m\e[1mcurrent.pot\e[0m\e[92m exists, downloading new sites\e[0m"
+	echo -e "\e[1mcurrent.pot\e[0m exists, downloading new sites..."
 	mv current.potfile old.potfile
 	./get-pot.sh "$KEY" > temp-pot.txt
 	cat temp-pot.txt | sort | uniq -w 12 | cut -d ":" -f 1,3,4 > current.potfile
 	rm temp-pot.txt
 	test -f "old.potfile"
 	diff -ua old.potfile current.potfile | grep -Ea "^\+" | tail -n +2 | tr -d "+" > newsites.txt
+	WC_LINES=$(wc -l newsites.txt | cut -d " " -f 1)
+	if [[ $WC_LINES == 0 ]]; then
+		echo "No new sites cracked."
 	else
-	echo -e "\e[33mNo previous potfiles detected, downloading sites\e[0m"
+		echo -e "\e[92m$WC_LINES new sites cracked.\e[0m"
+	fi
+	else
+	echo -e "\e[33mNo previous potfiles detected, downloading sites...\e[0m"
 	./get-pot.sh "$KEY" > temp-pot.txt
 	cat temp-pot.txt | sort | uniq -w 12 | cut -d ":" -f 1,3,4 > current.potfile
 	rm temp-pot.txt
