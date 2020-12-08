312d56fdc805892c316d20e53e7a88517f6b5d25
Czechball <david@jenne.cz>
2020-12-08 10:10:10 +0100

    fixed syntax

--- sort-pot.sh
+++ sort-pot.sh
@@ -20,12 +20,12 @@ if [[ $1 == "" ]]; then
 			if test -f "old.potfile"; then
 				mkdir -p archive
 				TEMPDATE=$(date -r old.potfile "+%Y-%m-%d_%H-%M-%S")
-				mv old.potfile archive/$TEMPDATE-old.potfile
+				mv old.potfile "archive/$TEMPDATE-old.potfile"
 			fi
 		echo -e "\e[1mcurrent.pot\e[0m exists, downloading new sites..."
 		mv current.potfile old.potfile
 		./get-pot.sh "$KEY" > temp-pot.txt
-		cat temp-pot.txt | sort | uniq -w 12 | cut -d ":" -f 1,3,4 > current.potfile
+		sort temp-pot.txt | uniq -w 12 | cut -d ":" -f 1,3,4 > current.potfile
 		rm temp-pot.txt
 		test -f "old.potfile"
 		diff -ua old.potfile current.potfile | grep -Ea "^\+" | tail -n +2 | tr -d "+" > newsites.txt
@@ -38,14 +38,14 @@ if [[ $1 == "" ]]; then
 		else
 		echo -e "\e[33mNo previous potfiles detected, downloading sites...\e[0m"
 		./get-pot.sh "$KEY" > temp-pot.txt
-		cat temp-pot.txt | sort | uniq -w 12 | cut -d ":" -f 1,3,4 > current.potfile
+		sort temp-pot.txt | uniq -w 12 | cut -d ":" -f 1,3,4 > current.potfile
 		rm temp-pot.txt
 		fi
 	fi
 else
-	if ( test -f $1 ); then
+	if test -f "$1"; then
 		echo "Parsing custom potfile: $1"
-		cat $1 | sort | uniq -w 12 | cut -d ":" -f 1,3,4 > parsed-$1
+		sort "$1" | uniq -w 12 | cut -d ":" -f 1,3,4 > "parsed-$1"
 	else
 		echo  "$1 is not a valid file"
 		exit
