a2f2d13c204091f160afd29ccbb1edc84ab1574e
Czechball <david@jenne.cz>
2020-12-15 11:39:30 +0100

    added zenity dialog for uploading a single file

--- upload-pcapng.sh
+++ upload-pcapng.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 VERSION="v1.2"
 SCRIPTPATH="$( cd "$(dirname "$0")" || { echo -e "\e[91mERROR\e[0m: Script path cannot be found" ; exit; } >/dev/null 2>&1 ; pwd -P )"
+GUI=false
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
 
 source "$SCRIPTPATH"/creds.txt
+
+show_usage ()
+{
+	echo "Invalid arguments. Usage: $0 <file> <options> Use $0 -h for help"
+	exit 1
+}
+
+zenity_error ()
+{
+	zenity --error --ellipsize --text="$1"
+	exit 1
+}
 
 if ping "wpa-sec.stanev.org" -c 1 -w 5 > /dev/null; then
 	:
@@ -11,7 +25,7 @@ else
 	exit
 fi
 
-while getopts "d:h?:*:" arg; do
+while getopts "d:hg?:*:" arg; do
 	case ${arg} in
 	d ) DIRECTORY="$OPTARG";;
 	h )
@@ -26,12 +40,21 @@ Options:\n\
 		echo
 		exit
 	;;
-	\? ) echo "Invalid arguments. Use $0 -h for help"; exit
+	g )
+		if which zenity >/dev/null; then
+			echo -e "Running in GUI mode..."
+			GUI=true
+		else
+			echo -e "\e[91mERROR\e[0m: Zenity not found."
+		fi
 	;;
-	* )  echo "Invalid arguments. Use $0 -h for help"; exit
+	\? ) show_usage
+	;;
+	* )  show_usage
 	;;
   esac
 done
+shift $(( OPTIND-1 ))
 
 if [[ $WPASECKEY == "" ]]; then
 	echo -e "\e[91mERROR\e[0m: No wpa-sec key supplied. Enter your key into creds.txt"
@@ -44,18 +67,42 @@ if [[ $DIRECTORY == "" ]]; then
 		echo -e "\e[91mERROR\e[0m: No file specified. Use $0 -h for help"
 		exit
 	fi
-		printf "Uploading %s... " "$FILE"
 		if test -f "$FILE"; then
-			RESULT=$(curl -s "https://wpa-sec.stanev.org/?submit" -X POST -F "file=@$FILE" -b "key=$WPASECKEY" -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36' 2>/dev/null)
-			if echo "$RESULT" | grep "No valid handshakes" > /dev/null; then
-				echo -e "\e[91mno valid handshakes/PMKIDs found.\e[0m"
-			elif echo "$RESULT" | grep "Not a valid capture file" > /dev/null; then
-				echo -e "\e[91mnot a valid capture file.\e[0m"
+			if [[ $GUI = true ]]; then
+				(curl "https://wpa-sec.stanev.org/?submit" -X POST -F "file=@$FILE"\
+					-b "key=$WPASECKEY"\
+					-A "$USER_AGENT" -# -o /tmp/curl-wpasec-output 2>&1 | while IFS= read -r -n1 char; do
+				    [[ $char =~ [0-9] ]] && keep=1 ;
+				    [[ $char == % ]] && echo "$progress" && progress="" && keep=0 ;
+				    [[ $keep == 1 ]] && progress="$progress$char" ;
+				done) | zenity --progress --title="Uploading..." --text="$FILE" --auto-kill --time-remaining --auto-close
+				RESULT=$(cat /tmp/curl-wpasec-output)
+				if echo "$RESULT" | grep "No valid handshakes" > /dev/null; then
+					zenity_error "No valid handshakes/PMKIDs found."
+				elif echo "$RESULT" | grep "Not a valid capture file" > /dev/null; then
+					zenity_error "Not a valid capture file."
+				elif echo "$RESULT" | grep "was already submitted" > /dev/null; then
+					zenity_error "File was already uploaded before."
+				fi
+				HS_COUNT=$(echo "$RESULT" | grep "EAPOL pairs (best)" | sed 's/[^0-9]*//g')
+				PM_COUNT=$(echo "$RESULT" | grep "PMKID (best)" | sed 's/[^0-9]*//g')
+				TOTAL_COUNT=$(( HS_COUNT + PM_COUNT ))
+				zenity --info --title="Success" --text="$TOTAL_COUNT handshakes uploaded from $FILE" --ellipsize
+			else
+				printf "Uploading %s... " "$FILE"
+				RESULT=$(curl -s "https://wpa-sec.stanev.org/?submit" -X POST -F "file=@$FILE" -b "key=$WPASECKEY" -A "$USER_AGENT" 2>/dev/null)
+				if echo "$RESULT" | grep "No valid handshakes" > /dev/null; then
+					echo -e "\e[91mno valid handshakes/PMKIDs found.\e[0m"
+				elif echo "$RESULT" | grep "Not a valid capture file" > /dev/null; then
+					echo -e "\e[91mnot a valid capture file.\e[0m"
+				elif echo "$RESULT" | grep "was already submitted" > /dev/null; then
+					echo -e "\e[91mfile was already uploaded before.\e[0m"
+				fi
+				HS_COUNT=$(echo "$RESULT" | grep "EAPOL pairs (best)" | sed 's/[^0-9]*//g')
+				PM_COUNT=$(echo "$RESULT" | grep "PMKID (best)" | sed 's/[^0-9]*//g')
+				TOTAL_COUNT=$(( HS_COUNT + PM_COUNT ))
+				echo -e "\e[92m$TOTAL_COUNT handshakes\e[0m"
 			fi
-			HS_COUNT=$(echo "$RESULT" | grep "EAPOL pairs (best)" | sed 's/[^0-9]*//g')
-			PM_COUNT=$(echo "$RESULT" | grep "PMKID (best)" | sed 's/[^0-9]*//g')
-			TOTAL_COUNT=$(( HS_COUNT + PM_COUNT ))
-			echo -e "\e[92m$TOTAL_COUNT handshakes\e[0m"
 		else
 			echo "$FILE is not a valid file. Aborting"
 		fi
@@ -74,7 +121,7 @@ else
 			PM_COUNT=""
 			C=$(( C + 1 ))
 			printf "Uploading %s... [%s/%s]: " "$FILE" "$C" "${#FILES[@]}"
-			RESULT=$(curl -s "https://wpa-sec.stanev.org/?submit" -X POST -F "file=@$FILE" -b "key=$WPASECKEY" -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36' 2>/dev/null)
+			RESULT=$(curl -s "https://wpa-sec.stanev.org/?submit" -X POST -F "file=@$FILE" -b "key=$WPASECKEY" -A "$USER_AGENT" 2>/dev/null)
 			if echo "$RESULT" | grep "No valid handshakes" > /dev/null; then
 				echo -e "\e[91mno valid handshakes/PMKIDs found.\e[0m"
 			elif echo "$RESULT" | grep "Not a valid capture file" > /dev/null; then
