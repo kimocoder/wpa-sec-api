520aa483c77bb014c4a1f24bfc39a3ccf1184e36
Czechball <david@jenne.cz>
2020-12-26 04:01:10 +0100

    new config system

--- get-pot.sh
+++ get-pot.sh
@@ -2,19 +2,27 @@
 VERSION="v1.2"
 SCRIPTPATH="$( cd "$(dirname "$0")" || { echo -e "\e[91mERROR\e[0m: Script path cannot be found" ; exit; } >/dev/null 2>&1 ; pwd -P )"
 USER_AGENT="get-pot $VERSION, part of wpa-sec-api by Czechball (https://github.com/Czechball/wpa-sec-api)"
+CONFIGFILE="$SCRIPTPATH"/config.txt
 
-source "$SCRIPTPATH"/creds.txt || { echo -e "\e[91mERROR\e[0m: creds.txt doesn't exist in scritp path" ; exit; }
-
-if ping "wpa-sec.stanev.org" -c 1 -w 5 > /dev/null; then
+if test -f "$CONFIGFILE"; then
 	:
 else
-	echo -e "\e[91mERROR\e[0m: wpa-sec.stanev.org couldn't be reached, check your internet connection"
+	echo "$CONFIGFILE doesn't exist, creating a new one using setup.sh"
+	"$SCRIPTPATH"/setup.sh
+fi
+
+source "$CONFIGFILE" || { echo -e "\e[91mERROR\e[0m: $CONFIGFILE doesn't exist in script path" ; exit; }
+
+if curl --head -s "$DWPAURL" >/dev/null; then
+	:
+else
+	echo -e "\e[91mERROR\e[0m: $DWPAURL couldn't be reached, check your internet connection"
 	exit
 fi
 
-if [[ $WPASECKEY == "" ]]; then
-	echo -e "\e[91mERROR\e[0m: No wpa-sec key supplied. Enter your key into creds.txt"
+if [[ $DWPAKEY == "" ]]; then
+	echo -e "\e[91mERROR\e[0m: No wpa-sec key supplied. Enter your key into $CONFIGFILE"
 	exit
 else
-	curl -s "https://wpa-sec.stanev.org/?api&dl=1" -b "key=$WPASECKEY" -A "$USER_AGENT"
+	curl -s "$DWPAURL/?api&dl=1" -b "key=$DWPAKEY" -A "$USER_AGENT"
 fi
